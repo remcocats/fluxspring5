65cb23729ccfa44bdc101bca3b54454b212461f5
JWA van Golverdinge Schut <Jaron.van.Golverdinge.Schut@rabobank.nl>
2018-03-03 18:58:27 +0100

    Handling Last-Event-ID header and skipping previously sent data

--- src/main/java/com/example/test/fluxjava8/Fluxjava8Application.java
+++ src/main/java/com/example/test/fluxjava8/Fluxjava8Application.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.http.codec.ServerSentEvent;
 
 import java.time.Duration;
 
 import reactor.core.publisher.Flux;
+
+import java.lang.NumberFormatException;
 
 @SpringBootApplication
 @RestController
@@ -27,8 +30,16 @@ public class Fluxjava8Application {
      * @return
      */
 	@GetMapping(value = "/api", produces = "text/event-stream")
-    public Flux<ServerSentEvent<Integer>> rootGet() {
-        Flux<java.lang.Integer> sampleFlux = Flux.just(1, 9, 4, 7, 6, 2, 2, 7, 3, 4, 8);
+    public Flux<ServerSentEvent<Integer>> rootGet(@RequestHeader(value="Last-Event-ID", required=false) String lastEventId) {
+	    int startId = 0;
+	    try {
+            startId = Integer.parseInt(lastEventId);
+        } catch (NumberFormatException e) {
+	        // Do nothing
+        }
+        System.out.println("Skipping: " + startId);
+
+        Flux<java.lang.Integer> sampleFlux = Flux.just(1, 9, 4, 7, 6, 2, 2, 7, 3, 4, 8).skip(startId);
 
         final Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
 
